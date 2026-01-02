e76434ba111fe2e2a59a0975b4aa9505cc3105d9
Bharat Panchal <bharatpanchal3578@gmail.com>
2026-01-02 23:38:11 +0530

    Implement product detail page with slug routing

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('product/<slug:slug>/', product_detail_view, name='product_detail'),
 ]
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
 
 # Create your views here.
@@ -6,3 +6,8 @@ from products.models import Product
 def home_view(request):
     products = Product.objects.filter(is_active=True)
     return render(request, 'products/home.html', {'products': products})
+
+def product_detail_view(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+
+    return render(request, 'products/product.html', {'product': product})
